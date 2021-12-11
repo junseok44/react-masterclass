bf49f125a5429e23abdc6fcadd6648537d6b19b6
unknown <newtonjjang@naver.com>
2021-12-11 16:27:09 +0900

    Updates

--- src/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components";
-
-// declare custom declaration file.
-declare module "styled-components" {
-  export interface DefaultTheme {
-    bgColor: string;
-    fontColor: string;
-  }
-}
